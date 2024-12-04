3741c11c72cdac71b5c672d694f1f680942d7098
JorgeAless <jcervantes841@gmail.com>
2024-12-04 15:50:32 -0600

    feat: Add colors console log messages

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import server from './server';
+import colors from "colors";
+import server from "./server";
 
 // Configurando el puerto con variable de entorno
 const port = process.env.PORT || 4000;
 
 // Iniciando el servidor...
 server.listen(port, () => {
-  console.log("Servidor funcionando en el puerto: ", port);
+  console.log(colors.green.bold(`Servidor funcionando en el puerto: ${port}`));
 });
 
