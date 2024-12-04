8e456cdd75739d0afb011c1ca5cf23811d4c7ced
JorgeAless <jcervantes841@gmail.com>
2024-12-04 08:58:05 -0600

    feat: Configuration Database

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import server from './server';
 
 // Configurando el puerto con variable de entorno
 const port = process.env.PORT || 4000;
+
 // Iniciando el servidor...
 server.listen(port, () => {
   console.log("Servidor funcionando en el puerto: ", port);
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
-import express from "express"; // ECSM Modules
+import express from "express";
+import 'dotenv/config';
 import router from "./router";
+import { ConnectDB } from "./config/db";
 // const express = require("express"); // Dependecias common js
 const app = express(); // Instancia del servidor
 
 // Leer formularios
 app.use(express.json());
+
+/* Conectando a la base de datos*/
+ConnectDB();
 // Routing
 app.use("/", router);
 
